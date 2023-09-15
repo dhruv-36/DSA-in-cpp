68eaf2c079b959ab28d6179fc0cd4d32329ccdc1
dhruv-36 <122593849+dhruv-36@users.noreply.github.com>
2023-09-15 19:22:35 +0530

    Update and rename rndom1.cpp to Arrays\Vectors.cpp

---
+++ Arrays\Vectors.cpp
@@ -0,0 +1,12 @@
+// void swap_alternates(int arr[], int size){
+//     for(int i=0;i<size;i=i+2){
+//         swap(arr[i],arr[i+1]);
+//         if(arr[i]==size-1){               // This is done in order to put the last element in the same order if(size is odd)
+//             break;
+//         }
+//     }
+
+//     for(int i=0;i<size;i++){
+//     cout<<arr[i]<<" ";
+//     }
+// }
