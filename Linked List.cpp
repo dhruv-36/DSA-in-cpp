87916c238221aedd5e84c11d6d4bafc2a8f0f54d
dhruv-36 <122593849+dhruv-36@users.noreply.github.com>
2024-08-03 21:51:32 +0530

    Update Linked List.cpp

--- Linked List.cpp
+++ Linked List.cpp
@@ -488,6 +488,64 @@ bool detectLoop(Node* head) {
 }
 
 
+
+
+Node* floydDetectLoop(Node* head) {
+
+    if(head == NULL)
+        return NULL;
+
+    Node* slow = head;
+    Node* fast = head;
+
+    while(slow != NULL && fast !=NULL) {
+        
+        fast = fast -> next;
+        if(fast != NULL) {
+            fast = fast -> next;
+        }
+
+        slow = slow -> next;
+
+        if(slow == fast) {
+            return slow;
+        }
+    }
+
+    return NULL;
+
+}
+
+Node* getStartingNode(Node* head) {
+
+    if(head == NULL) 
+        return NULL;
+
+    Node* intersection = floydDetectLoop(head);
+    
+    if(intersection == NULL)
+        return NULL;
+    
+    Node* slow = head;
+
+    while(slow != intersection) {
+        slow = slow -> next;
+        intersection = intersection -> next;
+    }  
+
+    return slow;
+
+}
+
+
+
+
+
+
+
+
+
+
 int main() {
 
     Node* tail = NULL;
