c1c2f1870d3f992e1fc29a2e48fcd9c1639c7c60
dhruv-36 <122593849+dhruv-36@users.noreply.github.com>
2024-08-04 19:59:13 +0530

    Update Linked List.cpp

--- Linked List.cpp
+++ Linked List.cpp
@@ -614,3 +614,74 @@ int main() {
 
 
 
+
+
+///// GFG Questions  //////////
+
+// Palindrome of Linked List
+class Solution {
+  public:
+    Node* mid(Node* head ) {
+        Node* slow = head;
+        Node* fast = head -> next;
+        
+        while(fast != NULL && fast-> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
+        }
+        
+        return slow;
+    }
+    Node* reverse(Node* head) {
+        
+        Node* curr = head;
+        Node* prev = NULL;
+        Node* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    // Function to check whether the list is palindrome.
+    bool isPalindrome(Node *head)
+    {
+        if(head -> next == NULL) {
+            return true;
+        }
+        
+        //step 1 -> find Middle
+        Node* middle = mid(head);
+        //cout << "Middle " << middle->data << endl;
+        
+        //step2 -> reverse List after Middle
+        Node* temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        //step3 - Compare both halves
+        Node* head1 = head;
+        Node* head2 = middle -> next;
+        
+        while(head2 != NULL) {
+            if(head2->data != head1->data) {
+                return 0;
+            }
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+        }
+    
+        //step4 - repeat step 2  restore the orignal linked list
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        return true;
+    }
+};
+
+
+
+
+
