a8edae6d3f0f94701ce57262cff4691cd83f8915
dhruv-36 <122593849+dhruv-36@users.noreply.github.com>
2024-09-10 20:59:45 +0530

    Update Accenture.cpp

--- Accenture.cpp
+++ Accenture.cpp
@@ -413,3 +413,22 @@ bool anagram(string a, string b) {
 
     return true;
 }
+
+
+
+
+///////////  MEDIAN of array  ////////////
+int main() {
+    // Write C++ code here
+    vector<int> arr={2,5,1,7};
+    sort(arr.begin(), arr.end());
+    float median;
+    if(arr.size()%2==1){
+        median= arr[arr.size()/2];
+    }
+    else{
+        median= (arr[arr.size()/2] + arr[arr.size()/2-1])/2.0;
+    }
+    cout<<median;
+    return 0;
+}
